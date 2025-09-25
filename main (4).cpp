0853263283acd2d3e72dbf0a8f66109e9a07c7ad
Agoni419 <s1120407@dmhs.kh.edu.tw>
2025-09-25 15:02:21 +0800

    作業2

---
+++ main (4).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// 華氏轉攝氏
+
+int main() {
+    double fahrenheit;
+    double celsius;
+
+    cout << "請輸入華氏溫度:\n";
+    cin >> fahrenheit;
+
+    celsius = (fahrenheit - 32) * 5 / 9;
+
+    cout << "攝氏溫度是:" << celsius << endl;
+
+    return 0;
+}
