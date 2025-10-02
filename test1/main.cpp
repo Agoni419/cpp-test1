721e47a73a72bf12ddc4297531b3dc7e7b1abbc0
Agoni419 <s1120407@dmhs.kh.edu.tw>
2025-10-02 05:57:24 +0000

    作業4

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-int main() {
-    std::cout<<"Hello world";
-    return 0;
+using namespace std;
 
+int main() {
+    int sum=0;
+    for (int i=1;i<=50;i++){
+        if(i%7)==0 cout<<i<<" ";
+    }
+    return 0;
 }
+
