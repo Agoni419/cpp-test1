d53e211dc7ad2113b0037e079dab5447d9f4959e
Agoni419 <s1120407@dmhs.kh.edu.tw>
2025-10-16 06:31:32 +0000

    hw5

---
+++ hw5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1;i<=6;++i){
+        for(int j=1;j<= i;++j){
+            cout<<"*";
+        }
+        cout << endl;
+    }
+    
+
+
+    int i;
+    int j;
+    i =1;
+    while (i<=9){
+        cout << i;
+        j = 1;
+        while(j <=9){
+            cout << i <<" * "<< j <<" = " << i * j <<"/t";
+            j +=1;
+        }
+        cout << endl;
+        i += 1;
+    }
+    return 0;
+}
